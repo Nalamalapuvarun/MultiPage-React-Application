f0fc4e10baa4c2ed9a1e137f1f0a30f01983fdeb
Nalamalapuvarun <varun.nalamalapu@gmail.com>
2020-04-26 23:14:05 -0400

    Updates

---
+++ precache-manifest.74751b5579c6e4c7c2f720dbf2336f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0dc7c9c4acc895d4dc5b22fd72230e3",
+    "url": "/MultiPage-React-Application/index.html"
+  },
+  {
+    "revision": "81d7a2311826809ad9c4",
+    "url": "/MultiPage-React-Application/static/css/2.56aa221b.chunk.css"
+  },
+  {
+    "revision": "81d7a2311826809ad9c4",
+    "url": "/MultiPage-React-Application/static/js/2.fafa878b.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/MultiPage-React-Application/static/js/2.fafa878b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82090982a9bce225306",
+    "url": "/MultiPage-React-Application/static/js/main.8f160504.chunk.js"
+  },
+  {
+    "revision": "452394636b65ea90a8ac",
+    "url": "/MultiPage-React-Application/static/js/runtime-main.77e1acd4.js"
+  }
+]);
