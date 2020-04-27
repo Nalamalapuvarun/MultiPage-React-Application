6b2dca737cc17eec1af4b4f8b14ebece6871e9fd
Nalamalapuvarun <varun.nalamalapu@gmail.com>
2020-04-26 23:28:44 -0400

    Added static courses as the jsonServer is not working on githubIO

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -6,8 +6,17 @@ import { Link } from "react-router-dom";
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
 
+  const courseValues = [
+    {
+      title: "Title1",
+      id: 1,
+      authorId: "Author 1",
+      category: "Category 1",
+    },
+  ];
+
   useEffect(() => {
-    getCourses().then(_courses => setCourses(_courses));
+    getCourses().then((_courses) => setCourses(_courses));
   }, []);
 
   return (
@@ -16,7 +25,7 @@ function CoursesPage() {
       <Link className="btn btn-primary" to="/course">
         Add Course
       </Link>
-      <CourseList courses={courses} />
+      <CourseList courses={courseValues} />
     </>
   );
 }
